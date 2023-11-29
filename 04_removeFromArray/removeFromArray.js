00e54279aa44990f73db25e045d6b6324beb8a0f
angel-vlzqz <106009767+angel-vlzqz@users.noreply.github.com>
2023-11-29 13:55:02 -0700

    solved exercixe 4. needed rest parameter to accept any number of arguments, and filter function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arrInput, ...valuesToRemove) {
+  let arr = arrInput;
+  return arr.filter((x) => !valuesToRemove.includes(x));
 };
 
 // Do not edit below this line
