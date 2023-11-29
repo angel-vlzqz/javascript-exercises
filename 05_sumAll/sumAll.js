63fc2ed7c3e0b841c464722cda26678b3ba2863a
angel-vlzqz <106009767+angel-vlzqz@users.noreply.github.com>
2023-11-29 14:21:20 -0700

    solved exercise 6

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (num1 < 0 || num2 < 0) return "ERROR";
+  if (!isNumber(num1) || !isNumber(num2)) return "ERROR";
+  if (num1 > num2) {
+    let temp = num1;
+    num1 = num2;
+    num2 = temp;
+  }
 
+  let sum = 0;
+  for (let i = num1; i < num2 + 1; i++) {
+    sum += i;
+  }
+  return sum;
 };
+
+function isNumber(num) {
+  return typeof num === "number";
+}
 
 // Do not edit below this line
 module.exports = sumAll;
