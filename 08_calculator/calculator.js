2941a08f955e2a7c0f07e938d012bac00f6912a0
angel-vlzqz <106009767+angel-vlzqz@users.noreply.github.com>
2024-01-29 17:50:39 -0700

    solved exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(...args) {
+  return args.reduce((total, current) => total + current, 0);
 };
 
-const subtract = function() {
-	
+const subtract = function(...args) {
+  let total = args[0];
+  for (let i = 1; i < args.length; i++) {
+    total -= args[i];
+  }
+  return total;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((total, current) => total + current, 0);};
+
+const multiply = function(array) {
+  return array.reduce((total, current) => total * current, 1);
 };
 
-const multiply = function() {
-
+const power = function(...args) {
+  return args.reduce((total, current) => total ** current);
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(value) {
+  if (value === 0) return 1;
+  let total = 1;
+  for (let i = value; i > 0; i--) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
