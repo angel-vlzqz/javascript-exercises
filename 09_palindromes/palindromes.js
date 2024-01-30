add0970f3c686a08e4a35440be50d3d46e7fc0ec
angel-vlzqz <106009767+angel-vlzqz@users.noreply.github.com>
2024-01-29 18:11:53 -0700

    solved fibonacci and title grab

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  // Remove spaces and punctuation
+  string = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
 
+  // Convert to lowercase
+  string = string.toLowerCase();
+
+  // works with multiple words
+  string = string.replace(/\s/g, "");
+
+  // Compare to reverse
+  let reverseString = string.split("").reverse().join("");
+  return string == reverseString;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,29 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+  // returns the nth number in the fibonacci sequence
+  // can be done recursively
+  // accepts only positive integers
+  // accepts strings
 
+  // convert input to integer
+  let num = parseInt(input);
+  // check if input is a number
+  // if not, return '0'
+  if (isNaN(num) || num < 0) {
+    return 'OOPS';
+  }
+  
+  // base case
+  if (num == 0) {
+    return 0;
+  }
+  else if (num == 1) {
+    return 1;
+  }
+
+  // recursive case
+  else {
+    return fibonacci(num - 1) + fibonacci(num - 2);
+  }
 };
 
 // Do not edit below this line
